746637f8c4145f7d8b31615c6bb6dd1d8bc8707b
nobug <376330019@qq.com>
2023-05-10 18:14:43 +0800

    2

--- mj.py
+++ mj.py
@@ -6,3 +6,5 @@ ENV mj.discord.user-token MTA3NTExMjg2NTQ4NjM0NDI0Mg.G2OvuV.vB_A5WHj9B9kd01iwc63
 ENV mj.discord.bot-token MTEwNTQ1MDM1MTI4MjM3Njc2NA.GA5RHw.bQF3HAKVc4PaQi5fYiOgS8FGfxj9LmkE3m3DxY
 
 EXPOSE 8080
+
+CMD ["midjourney-proxy"]
