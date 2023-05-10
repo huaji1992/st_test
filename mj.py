be6291a2079e2eb135b5429d47db4ccb2692da88
nobug <376330019@qq.com>
2023-05-10 20:17:05 +0800

    1

--- mj.py
+++ mj.py
@@ -1,6 +1,6 @@
 FROM novicezk/midjourney-proxy
 
-ENV mj.discord.guild-id 1104787197980319916 mj.discord.channel-id 1104787197980319919 mj.discord.user-token MTA3NTExMjg2NTQ4NjM0NDI0Mg.G2OvuV.vB_A5WHj9B9kd01iwc63iWyfQZx-8TSaW-AmMM mj.discord.bot-token MTEwNTQ1MDM1MTI4MjM3Njc2NA.GA5RHw.bQF3HAKVc4PaQi5fYiOgS8FGfxj9LmkE3m3DxY
+ENV mj.discord.guild-id 1105469098915266620 mj.discord.channel-id 1105469099884167271 mj.discord.user-token MTEwNTQyOTM5MjI2MTQwMjY4NA.G9ijOL.Zs3N2Yarwx-mz_0hVbLbSEIVwYRqiAMN4k6Fnc mj.discord.bot-token MTEwNTgyNjk4MDYyNjE3NDAzMg.Gykyvt.3CEQVLMSMfZ3To6BCu4dw_a56MwlfsqV6qhiP0
 
 EXPOSE 8080
 
